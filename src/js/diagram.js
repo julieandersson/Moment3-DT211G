3aa16690a340044d494a12f68a393c7b2d1d0446
julieandersson <juan2301@student.miun.se>
2024-02-24 15:09:32 +0200

    ändrar htmlfil och hämtar huvudmodel och färg från chartjsbiblioteket

---
+++ src/js/diagram.js
@@ -0,0 +1,8 @@
+"use strict";
+
+// Hämtar huvudmodulen för chartjsbiblioteket
+import Chart from 'chart.js/auto';
+//Hämtar färgfunktionaliteten från chartjsbiblioteket
+import { Colors } from 'chart.js';
+
+Chart.register(Colors);
