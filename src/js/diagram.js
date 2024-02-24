947e057b971889a4ba68d1e1668f74e4a9412b0d
julieandersson <juan2301@student.miun.se>
2024-02-24 16:09:04 +0200

    lägger in ett stapeldiagram och ett cirkeldiagram

--- src/js/diagram.js
+++ src/js/diagram.js
@@ -6,3 +6,50 @@ import Chart from 'chart.js/auto';
 import { Colors } from 'chart.js';
 
 Chart.register(Colors);
+
+
+// Stapeldiagram 
+
+const ctx = document.getElementById('barChart');
+
+const barChart = new Chart(ctx, {
+    type: 'bar',
+    data: {
+      labels: ['Red', 'Blue', 'Yellow', 'Green', 'Purple', 'Orange'],
+      datasets: [{
+        label: '# of Votes',
+        data: [12, 19, 3, 5, 2, 3],
+        borderWidth: 1
+      }]
+    },
+    options: {
+      scales: {
+        y: {
+          beginAtZero: true
+        }
+      }
+    }
+  });
+
+// Cirkeldiagram
+
+const ctx2 = document.getElementById('pieChart');
+
+const pieChart = new Chart(ctx2, {
+    type: 'pie',
+    data: {
+      labels: ['Red', 'Blue', 'Yellow', 'Green', 'Purple', 'Orange'],
+      datasets: [{
+        label: '# of Votes',
+        data: [12, 19, 3, 5, 2, 3],
+        borderWidth: 1
+      }]
+    },
+    options: {
+      scales: {
+        y: {
+          beginAtZero: true
+        }
+      }
+    }
+  });
