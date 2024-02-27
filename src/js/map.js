a8c94b294e253577dfcae13f9c44e894cb992c41
julieandersson <juan2301@student.miun.se>
2024-02-27 16:17:57 +0200

    tar bort kartan för att börja om, adderar ett searchform-id i htmlfilen för sökfunktionen

--- src/js/map.js
+++ src/js/map.js
@@ -1,52 +1,7 @@
-/* Karta hämtad med Leaflet.js och OpenStreetMap */
+/* Karta hämtad med Leaflet.js kombinerad med Nominatim API med OpenStreetMap */
 
 "use strict";
 
 
 let map = L.map('map');
 map.setView([51.505, -0.09], 13);
-
-L.tileLayer('https://tile.openstreetmap.org/{z}/{x}/{y}.png', {
-    maxZoom: 19,
-    attribution: '&copy; <a href="http://www.openstreetmap.org/copyright">OpenStreetMap</a>'
-}).addTo(map);
-
-L.Control.geocoder().addTo(map);
-
-
-// Visa nuvarande plats för användaren
-navigator.geolocation.watchPosition(success, error);
-
-let marker, circle, zoomed; 
-
-function success(pos) {
-
-    const lat = pos.coords.latitude;
-    const lng = pos.coords.longitude;
-    const accuracy = pos.coords.accuracy;
-
-    // Om användaren byter plats, uppdatera cirkel och pil till rätt/ny plats
-    if (marker) {
-        map.removeLayer(marker);
-        map.removeLayer(circle);
-    }
-
-    marker = L.marker([lat, lng]).addTo(map);
-    circle = L.circle([lat, lng], { radius: accuracy }).addTo(map);
-
-    if (!zoomed) {
-       zoomed = map.fitBounds(circle.getBounds());
-    }
-    
-    map.setView([lat, lng]);
-
-}
-
-function error (err) {
-
-    if (err.code === 1) {
-        alert("Vänligen godkänn geolocation access");
-    } else {
-        alert("Kan inte hämta nuvarande plats");
-    }
-}
