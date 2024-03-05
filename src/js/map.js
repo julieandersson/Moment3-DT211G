48a63d4ce8d933f3e6142f849c21d420abc5905b
julieandersson <juan2301@student.miun.se>
2024-03-05 19:29:31 +0200

    Skapar en karta med openlayers och en attributionskontroll

--- src/js/map.js
+++ src/js/map.js
@@ -2,3 +2,26 @@
 
 "use strict";
 
+var attribution = new ol.control.Attribution({
+    collapsible: false
+});
+
+var map = new ol.Map({
+    controls: ol.control.defaults({attribution: false}).extend([attribution]),
+    layers: [
+        new ol.layer.Tile({
+            source: new ol.source.OSM({
+                url: 'https://{a-c}.tile.openstreetmap.org/{z}/{x}/{y}.png',
+                attributions: [ ol.source.OSM.ATTRIBUTION, 'Tiles courtesy of <a href="https://geo6.be/">GEO-6</a>' ],
+                maxZoom: 18
+            })
+        })
+    ],
+    target: 'map',
+    view: new ol.View({
+        center: ol.proj.fromLonLat([18.063240, 59.334591]),
+        maxZoom: 18,
+        zoom: 12
+    })
+});
+
